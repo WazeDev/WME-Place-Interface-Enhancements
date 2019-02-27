1c7d2f89a431cec3ce9f573fba5890fbead60541
justins83 <justins83.waze@gmail.com>
2019-02-27 09:19:02 -0500

    Update WMEPIE.js
    Incrementing version to pull the latest WazeWrap

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2019.02.18.01
+// @version      2019.02.27.01
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -50,7 +50,7 @@ var UpdateObject, MultiAction;
     let hoursparser;
     let GLE;
     var catalog = [];
-    const updateMessage = "Dogfooding. Nom nom nom.  Added functionality to WazeWrap to handle some error catching so WME updates cause less trouble when something is broken.";
+    const updateMessage = "Incrementing the version to pull the latest WazeWrap which restores the missing functions removed in the latest WME update and are causing issues with scripts.";
 
     //Layer definitions
     {
