0b45b2cc6d072ba812eb3099054a8fd724e8f078
justins83 <justins83.waze@gmail.com>
2019-03-07 14:47:06 -0500

    Update WMEPIE.js
    Bug fix for lonlat.toPoint

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2019.02.28.02
+// @version      2019.03.07.01
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -50,7 +50,7 @@ var UpdateObject, MultiAction;
     let hoursparser;
     let GLE;
     var catalog = [];
-    const updateMessage = "Further backend changes are causing issues with the Google Link Enhancer feature.  This has been disabled in order to prevent further issues, until the functionality can reliably be restored.";
+    const updateMessage = "Bug fix from WME update.";
 
     //Layer definitions
     {
@@ -837,7 +837,7 @@ var UpdateObject, MultiAction;
         registerEvents(AddMakePrimaryButtons);
         AddMakePrimaryButtons();
 
-        WazeWrap.Interface.ShowScriptUpdate("WME Place Interface Enhancements", GM_info.script.version, updateMessage, "https://www.waze.com/forum/viewtopic.php?f=819&t=215990");
+        WazeWrap.Interface.ShowScriptUpdate("WME Place Interface Enhancements", GM_info.script.version, updateMessage, "", "https://www.waze.com/forum/viewtopic.php?f=819&t=215990");
     }
 
     function SetupPhotoViewer(){
@@ -1572,7 +1572,7 @@ var UpdateObject, MultiAction;
 
     function handleNavPointOffScreen() {
 		if (selectedItem !== WazeWrap.getSelectedFeatures().first() ||
-			WazeWrap.Geometry.isGeometryInMapExtent(ClosestSegmentNavPoint.lonlat.toPoint())) {
+			WazeWrap.Geometry.isGeometryInMapExtent(new OL.Geometry.Point(ClosestSegmentNavPoint.lonlat.lon, ClosestSegmentNavPoint.lonlat.lat))) {
 			W.map.events.unregister('moveend', window, handleNavPointOffScreen);
 			checkSelection();
 		}
@@ -1606,7 +1606,7 @@ var UpdateObject, MultiAction;
 	function findNearestSegment(navPoint) {
         var closestSegment = {};
         if(navPoint.element)
-            navPoint = W.geometryEditing.activeEditor._navigationPointMarker.lonlat.toPoint();
+            navPoint = new OL.Geometry.Point(W.geometryEditing.activeEditor._navigationPointMarker.lonlat.lon, W.geometryEditing.activeEditor._navigationPointMarker.lonlat.lat);
 
         closestSegment = WazeWrap.Geometry.findClosestSegment(navPoint,false, false);
 
