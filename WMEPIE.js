6f6f0622a4cebef4c2b285bf62c8c8db3d6bee2b
justins83 <justins83.waze@gmail.com>
2019-03-14 08:59:10 -0400

    Update WMEPIE.js
    Fixing typo in unregistering an event when disabling Show Closest Segment

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2019.03.12.02
+// @version      2019.03.14.01
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -50,7 +50,7 @@ var UpdateObject, MultiAction;
     let hoursparser;
     let GLE;
     var catalog = [];
-    const updateMessage = "Bug fix from WME update.";
+    const updateMessage = "Fixing typo in unregistering an event when disabling Show Closest Segment.";
 
     //Layer definitions
     {
@@ -413,7 +413,7 @@ var UpdateObject, MultiAction;
                 W.model.venues.on('objectschanged', ObjectsChanged);
             }
             else{
-                WazeWrap.Eventsun.register('afterundoaction', this, checkSelection);
+                WazeWrap.Events.unregister('afterundoaction', this, checkSelection);
                 WazeWrap.Events.unregister('afteraction', this, checkSelection);
                 WazeWrap.Events.unregister('selectionchanged', this, checkSelection);
                 W.model.venues.off('objectschanged', ObjectsChanged);
