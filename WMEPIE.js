04c9f70a8f89da5bb7201cb3712e29cd00c98bd7
justins83 <justins83.waze@gmail.com>
2019-06-12 14:11:15 -0400

    Update WMEPIE.js

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2019.06.12.01
+// @version      2019.06.12.02
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -3600,7 +3600,7 @@ var UpdateObject, MultiAction;
             if (!settings.hasOwnProperty(prop))
                 settings[prop] = defaultSettings[prop];
         }*/
-    	settings = $.extend({}, defaultSettings, settings);
+    	settings = $.extend({}, defaultSettings, loadedSettings);
 
         let serverSettings = await WazeWrap.Remote.RetrieveSettings("WME_PIE");
         if(serverSettings && serverSettings.lastSaved > settings.lastSaved)
