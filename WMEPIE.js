2d2cda19b3709c6998f94792c26ac3b64126608b
justins83 <justins83.waze@gmail.com>
2020-06-01 11:20:03 -0400

    Update WMEPIE.js
    Forcing update of GLE

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2020.06.01.04
+// @version      2020.06.01.05
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
