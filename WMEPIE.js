a80f5df947464e0755c82f4f35b59abb3fc7affc
MapOMatic <mapomatic.waze@gmail.com>
2023-07-27 18:19:26 -0400

    Update WMEPIE.js
    GLE fixed. Bump version # and change updateMessage.

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2023.07.27.01
+// @version      2023.07.27.02
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -52,7 +52,7 @@ var UpdateObject, MultiAction;
     let hoursparser;
     let GLE;
     var catalog = [];
-    const updateMessage = "";
+    const updateMessage = "Google Link Enhancer is fixed";
     var lastSelectedFeature;
     const SCRIPT_VERSION = GM_info.script.version.toString();
     const SCRIPT_NAME = GM_info.script.name;
